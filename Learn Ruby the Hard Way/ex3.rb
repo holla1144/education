54226f172b3668c2f06aae02115ea2a244f08b71
Max Holland <holla1144@gmail.com>
2018-07-15 16:53:02 +0300

    First lessons in Learn Ruby the Hard Way

---
+++ Learn Ruby the Hard Way/ex3.rb
@@ -0,0 +1,36 @@
+puts "I will now count my chickens:"
+
+# divides 30 by 6 and adds the product to 25
+puts "Hens #{25 + 30 / 6}"
+
+# multiples 25 by 3, gets the remainder of the product's division by four, 
+# and subtracts it from 100
+puts "Rooster #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7"
+
+# 5 < -2? False
+puts 3 + 2 < 5 - 7
+
+# Prints 5
+puts "What is 3 + 2? #{3 + 2}"
+
+# Prints -2 
+puts "What is 5 - 7 #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more?"
+
+# Prints true
+puts "Is it greater? #{5 > -2}"
+
+# Prints true
+puts "Is it greater or equal? #{5 >= -2}"
+
+# Prints false
+puts "Is it less or equal? #{5 <= 2}"
